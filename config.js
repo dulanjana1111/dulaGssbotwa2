ff95e70903a2a014a052ac38e1ce7d3498af7e79
dulanjana1111 <144605737+dulanjana1111@users.noreply.github.com>
2024-03-22 01:57:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -22,11 +22,11 @@ global.APIs = {
 
 // Other
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = '𝑆𝛪𝐷 𝐵𝛨𝛥𝛪';
-global.owner = ['917050906659'];
-global.premium = ['917050906659'];
-global.packname = '𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢';
-global.author = '𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫';
+global.ownername = 'x-Dula & X-SaHan';
+global.owner = ['94763909334,94742314485'];
+global.premium = ['94763909334,94742314485'];
+global.packname = '𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢 Dula & Sahan';
+global.author = 'Dula_X & Sahan.x-MD';
 global.sessionName = 'session';
 global.prefa = ['!', '.'];
 global.sp = '✪';
